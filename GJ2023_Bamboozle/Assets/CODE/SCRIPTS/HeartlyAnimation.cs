0e037a301e4323859b0e7abd3d820e0ada1b260f
Diego Garcia Figueroa <diegogarciafigueroa17@gmail.com>
2023-02-04 23:46:16 -0600

    Added Heartly Sprite Animation in My Test Scene

---
+++ GJ2023_Bamboozle/Assets/CODE/SCRIPTS/HeartlyAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartlyAnimation : MonoBehaviour
+{
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    public float switchInterval = 1.0f;  // time interval for switching between sprites
+
+    private SpriteRenderer spriteRenderer;
+    private float elapsedTime = 0.0f;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite1;
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= switchInterval)
+        {
+            elapsedTime = 0.0f;
+
+            // switch between sprites
+            if (spriteRenderer.sprite == sprite1)
+            {
+                spriteRenderer.sprite = sprite2;
+            }
+            else
+            {
+                spriteRenderer.sprite = sprite1;
+            }
+        }
+    }
+}
